2fde88d87259503f81bb6002d9002beaa0565d41
jamespilcher <pilcherjames0@gmail.com>
2022-06-24 20:31:03 +0100

    commit

--- isoBuilder/res/main.js
+++ isoBuilder/res/main.js
@@ -9,3 +9,52 @@ const origin = [gridWidth / 2,1]
 const world = new Array(gridHeight).fill(0).map(() => new Array(gridWidth).fill(0));
 // assume aspect ratio of tile is 2:1
 // 64 to 32
+const texture = new Image()
+texture.src = "textures/01_130x66_130x230.png"
+texture.onload = _ => init()
+
+const init = () => {
+
+	tool = [0,0]
+
+	map = [
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],
+		[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+	]
+
+	canvas = $("#bg")
+	canvas.width = 910
+	canvas.height = 666
+	w = 910
+	h = 462
+	texWidth = 12
+	texHeight = 6
+	bg = canvas.getContext("2d")
+	ntiles = 7
+	tileWidth = 128
+	tileHeight = 64
+	bg.translate(w/2,tileHeight*2)
+
+	loadHashState(document.location.hash.substring(1))
+
+	drawMap()
+
+	fg = $('#fg')
+	fg.width = canvas.width
+	fg.height = canvas.height
+	cf = fg.getContext('2d')
+	cf.translate(w/2,tileHeight*2)
+	fg.addEventListener('mousemove', viz)
+	fg.addEventListener('contextmenu', e => e.preventDefault())
+	fg.addEventListener('mouseup', unclick)
+	fg.addEventListener('mousedown', click)
+	fg.addEventListener('touchend', click)
+	fg.addEventListener('pointerup', click)
+
+	tools = $('#tools')
+}
