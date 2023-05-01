30c1a9a1089f069bc6d0e8eab1c0de6785d23704
jamespilcher <pilcherjames0@gmail.com>
2023-05-01 16:14:52 +0100

    created navbar

---
+++ navbar.js
@@ -0,0 +1,13 @@
+document.write(`
+
+<nav>
+    <ul class="left">
+        <a href="/index.html" class="logo">J<span style="color: lightblue;">.</span>Pilcher</a>
+    </ul>
+    <ul>
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="/about.html">About</a></li>
+    </ul>
+</nav>
+
+`);
