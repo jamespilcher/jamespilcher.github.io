a0c9e1a4dfd89bc3b3a38b5bf2a83c1898bafe1a
jamespilcher <pilcherjames0@gmail.com>
2023-05-01 17:01:51 +0100

    navbar style changes. Added index, music, projects

--- navbar.js
+++ navbar.js
@@ -2,11 +2,14 @@ document.write(`
 
 <nav>
     <ul class="left">
-        <a href="/index.html" class="logo">J<span style="color: lightblue;">.</span>Pilcher</a>
+        <a href="/index.html" class="logo">J<span style="color: RGB(146, 168, 209);">.</span>Pilcher</a>
     </ul>
     <ul>
-        <li><a href="/index.html">Home</a></li>
         <li><a href="/about.html">About</a></li>
+        <li><a href="/projects.html">Projects</a></li>
+        <li><a href="/music.html">Music</a></li>
+
+
     </ul>
 </nav>
 
