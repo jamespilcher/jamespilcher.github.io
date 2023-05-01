8f7a29c8be330e0cddb3d188ef91abb8cddad8f3
jamespilcher <pilcherjames0@gmail.com>
2023-05-01 22:02:02 +0100

    fixed file paths

--- navbar.js
+++ navbar.js
@@ -2,12 +2,12 @@ document.write(`
 
 <nav>
     <ul class="left">
-        <a href="/index.html" class="logo">J<span style="color: RGB(146, 168, 209);">.</span>Pilcher</a>
+        <a href="../home" class="logo">J<span style="color: RGB(146, 168, 209);">.</span>Pilcher</a>
     </ul>
     <ul>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/projects.html">Projects</a></li>
-        <li><a href="/music.html">Music</a></li>
+        <li><a href="../blog">Blog</a></li>
+        <li><a href="../projects">Projects</a></li>
+        <li><a href="../music">Music</a></li>
     </ul>
 </nav>
 
