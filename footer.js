c07062094a012efc5be91923810eb0226d24f3ab
jamespilcher <pilcherjames0@gmail.com>
2023-05-01 21:52:04 +0100

    footer

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.write(`
 
 <footer>
     <ul>
-        <li><a href="username.github.io/res/JPResume.pdf" target="_blank">Resume</a>
+        <li><a href="jamespilcher.github.io/res/JPResume.pdf" target="_blank">Resume</a>
         <li><a href="/projects.html">LinkedIn</a></li>
         <li><a href="/music.html">GitHub</a></li>
         <li><a href="/music.html">Youtube</a></li>
