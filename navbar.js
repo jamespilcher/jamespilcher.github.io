1d30775e0e088b645e78fab052310342362b8535
jamespilcher <pilcherjames0@gmail.com>
2023-05-02 16:28:14 +0100

    tetris for site. Also favicon!

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.write(`
 
 <nav>
     <ul class="left">
-        <a href="../home" class="logo">J<span style="color: RGB(146, 168, 209);">.</span>Pilcher</a>
+        <a href="../home" class="logo">J<span style="color: RGB(200, 70, 70);">.</span>Pilcher</a>
     </ul>
     <ul>
         <li><a href="../blog">Blog</a></li>
