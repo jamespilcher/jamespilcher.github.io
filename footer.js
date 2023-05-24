1d1cb70fa384863bac0284dd30946c55f460a9ee
jamespilcher <pilcherjames0@gmail.com>
2023-05-24 14:23:59 +0100

    fun now 'fun.js', removed yt from footer

--- footer.js
+++ footer.js
@@ -3,10 +3,9 @@ document.write(`
 <footer>
     <ul>
         <li><a href="../res/JPResume.pdf" target="_blank">Resume</a>
-        <li><a href="/projects.html">LinkedIn</a></li>
-        <li><a href="/music.html">GitHub</a></li>
-        <li><a href="/music.html">Youtube</a></li>
-        <li><a href="/music.html">Spotify</a></li>
+        <li><a href="https://www.linkedin.com/in/jpilcher0/" target="_blank">LinkedIn</a></li>
+        <li><a href="https://github.com/jamespilcher" target="_blank">GitHub</a></li>
+        <li><a href="https://open.spotify.com/artist/7hbK21OWNLxqGvtKRQtNdH?si=Nze_t8k7Sl6FLqDXszCMyg" target="_blank">Spotify</a></li>
     </ul>
 </footer>
 
