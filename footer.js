c87aee9b808da42fd43cfba02de1570922dca37c
jamespilcher <pilcherjames0@gmail.com>
2023-07-12 23:18:10 +0900

    added music descriptions

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.write(`
 
 <footer>
     <ul>
-        <li><a href="../res/JPResume.pdf" target="_blank">Resume</a>
+        <li><a href="../res/JP_RESUME_2023.pdf" target="_blank">Resume</a>
         <li><a href="https://www.linkedin.com/in/jpilcher0/" target="_blank">LinkedIn</a></li>
         <li><a href="https://github.com/jamespilcher" target="_blank">GitHub</a></li>
         <li><a href="https://open.spotify.com/artist/7hbK21OWNLxqGvtKRQtNdH?si=Nze_t8k7Sl6FLqDXszCMyg" target="_blank">Spotify</a></li>
