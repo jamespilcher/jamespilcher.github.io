c5c8007f5aed057b3671dc2981610fd81282140e
jamespilcher <pilcherjames0@gmail.com>
2023-08-29 23:42:11 +0900

    new font designs

--- navbar.js
+++ navbar.js
@@ -1,10 +1,20 @@
 document.write(`
+<link href="https://fonts.cdnfonts.com/css/pp-neue-montreal" rel="stylesheet">
+                                                
+<link href="https://fonts.cdnfonts.com/css/super-dream" rel="stylesheet">
+<link href="https://fonts.cdnfonts.com/css/scrouble-outline" rel="stylesheet">
 
 <nav>
     <ul class="left">
-        <a href="../home" class="logo">JPilcher<img src="../res/jim.png" class="logoimage"/></a>
+        <a href="../home" class="logo" style="
+        font-family: 'Super Dream', sans-serif;
+        " 
+        >JPilcher<img src="../res/jim.png" class="logoimage"/></a>
     </ul>
-    <ul>
+    <ul style="
+    font-family: 'Super Dream', sans-serif;
+    " 
+    >
         <li><a href="../blog">blog</a></li>
         <li><a href="../projects">codeArt</a></li>
         <li><a href="../music">music</a></li>
