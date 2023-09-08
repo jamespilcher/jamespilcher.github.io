27f85269147c838fe28d95f9d862707c66b450f3
James Pilcher <james.pilcher@rapyuta-robotics.com>
2023-09-08 16:13:30 +0900

    site tree restructure

--- navbar.js
+++ navbar.js
@@ -1,3 +1,6 @@
+let currentUrl = window.location.href;
+root = currentUrl.split("/").slice(0, 3).join("/");
+
 document.write(`
 <link href="https://fonts.cdnfonts.com/css/pp-neue-montreal" rel="stylesheet">
                                                 
@@ -5,13 +8,13 @@ document.write(`
 
 <nav>
     <ul class="left">
-        <a href="../home" class="logo"
-        >JPilcher<img src="../res/jim.png" class="logoimage"/></a>
+        <a href="${root}/home" class="logo"
+        >JPilcher<img src="${root}/res/jim.png" class="logoimage"/></a>
     </ul>
     <ul>
-        <li><a href="../blog">blog</a></li>
-        <li><a href="../codeArt">codeArt</a></li>
-        <li><a href="../music">music</a></li>
+        <li><a href="${root}/blog">blog</a></li>
+        <li><a href="${root}/codeArt">codeArt</a></li>
+        <li><a href="${root}/music">music</a></li>
     </ul>
 </nav>
 
