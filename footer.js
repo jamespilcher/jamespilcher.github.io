33cb595438ae957e842965410bdbd9e8805b15f0
jamespilcher <pilcherjames0@gmail.com>
2023-12-18 22:54:04 +0900

    rm resume from footer

--- footer.js
+++ footer.js
@@ -2,7 +2,6 @@ document.write(`
 
 <footer>
     <ul>
-        <li><a href="${root}/res/jamespilcher_cv.pdf" target="_blank">Resume</a>
         <li><a href="https://www.linkedin.com/in/jpilcher0/" target="_blank">LinkedIn</a></li>
         <li><a href="https://github.com/jamespilcher" target="_blank">GitHub</a></li>
         <li><a href="https://open.spotify.com/artist/7hbK21OWNLxqGvtKRQtNdH?si=Nze_t8k7Sl6FLqDXszCMyg" target="_blank">Spotify</a></li>
@@ -10,3 +9,5 @@ document.write(`
 </footer>
 
 `);
+
+// <li><a href="${root}/res/jamespilcher_cv.pdf" target="_blank">Resume</a>
