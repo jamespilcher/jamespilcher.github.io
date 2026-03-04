895f40d426d3fd634ec44113a7fa6956b7969795
jamespilcher <pilcherjames0@gmail.com>
2026-03-04 14:55:00 +0000

    update navbar

--- navbar.js
+++ navbar.js
@@ -1,10 +1,29 @@
 var currentUrl = window.location.href;
+
 root = currentUrl.split("/").slice(0, 3).join("/");
 
 jpilcher = "JPilcher"
 // if window size < 600px, use JP
 if (window.innerWidth < 600) {
     jpilcher = "JP"
+}
+
+// Use font-size for navbar scaling if window width < 800px
+if (window.innerWidth < 800) {
+    document.write(`<style>
+        nav, nav ul, nav li {
+            font-size: 0.9em !important;
+        }
+        nav .logo {
+            font-size: 2em !important;
+        }
+        .logoimage {
+            position: absolute;
+            top: 1.2rem;
+            height: 1.5rem;
+            padding-left: .5rem;
+        }
+    </style>`);
 }
 
 document.write(`
@@ -21,6 +40,7 @@ document.write(`
     <ul>
         <li><a href="${root}/codeArt">codeArt</a></li>
         <li><a href="https://roundapples.substack.com/t/poetry" target="_blank">poetry</a></li>
+        <li><a href="https://open.spotify.com/artist/7hbK21OWNLxqGvtKRQtNdH?si=xihJsVwhRZ6ob56-2kU71w" target="_blank">music</a></li>
         <li><button id="radioPlayer" onclick="playRadio()" data-tooltip="Unmute Me!"><div id="radioEmoji">📻🔇</div></button></li>    
     </ul>
 </nav>
